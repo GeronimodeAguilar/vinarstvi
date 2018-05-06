8af97862889bdcefbc126534ebfbe47e27270f6c
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-05-07 00:06:26 +0200

    video background + moving logo
    video background,moving logo,javascript,css,html fixes + pictures

--- scripts/before/slidevideo.js
+++ scripts/before/slidevideo.js
@@ -1,25 +1,5 @@
 'use strict';
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 9000); // Change image every 2 seconds
-}
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -111,3 +91,21 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 }
+
+
+const hero = document.querySelector('.picbox');
+const logo = hero.querySelector('.pic');
+    
+function logoMove(e) {
+ 
+const width = hero.offsetWidth;  
+const height = hero.offsetHeight;  
+let x = e.offsetX / 180;
+let y = e.offsetY / 180;    
+
+logo.style.transform = `
+translate(${x}%, ${y}%) skew(${x}deg,${-y}deg)
+`;
+}    
+    
+hero.addEventListener('mousemove', logoMove);    
